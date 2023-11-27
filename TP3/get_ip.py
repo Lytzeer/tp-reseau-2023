e592e0dd6117825cf2c8cb5b3ed5301be301775d
Lytzeer <101729154+Lytzeer@users.noreply.github.com>
2023-11-27 10:33:45 +0100

    Update get_ip.py

--- TP3/get_ip.py
+++ TP3/get_ip.py
@@ -4,7 +4,8 @@ import psutil
 
 def ip():
     if(os.name=="posix"):
-        return(psutil.net_if_addrs()[list(psutil.net_if_addrs().keys())[1]][0][1])
+        print(psutil.net_if_addrs()[list(psutil.net_if_addrs().keys())[1]][0][1])
+        return
     else:
         print(psutil.net_if_addrs()["Wi-Fi"][1][1])
     
